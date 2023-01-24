bca550ee0598c45fdbeeee280404265b7f808025
Harsh Pandey <coding.harshp@gmail.com>
2023-01-24 12:57:32 +0530

    Exp and Projects UI

--- components/Developer.jsx
+++ components/Developer.jsx
@@ -38,6 +38,66 @@ export default function Developer({ developer }) {
           </div>
         </div>
       </div>
+      <div className={styles.skills}>
+        {["Reactjs", "Nextjs", "Hardhat", "Etherjs", "Redux", "Electonjs"].map(
+          (skill, _i) => (
+            <span key={_i}>{skill}</span>
+          )
+        )}
+      </div>
+      <div className={styles.experience}>
+        <p>Experiences</p>
+        <div className={styles.exps}>
+          <ExperienceCard />
+          <ExperienceCard />
+          <ExperienceCard />
+        </div>
+      </div>
+
+      <div className={styles.experience}>
+        <p>Projects</p>
+        <div className={styles.exps}>
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ExperienceCard() {
+  return (
+    <div className={styles.expCard}>
+      <p>
+        TenX Tools <span>| Front-End Web3 Developer</span>
+      </p>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni labore
+        sit inventore cumque deleniti distinctio vitae asperiores earum porro
+        eaque facilis ea culpa, est excepturi id cumque itaque esse doloremque!
+      </p>
+    </div>
+  );
+}
+
+function ProjectCard() {
+  return (
+    <div className={styles.expCard}>
+      <p>
+        Dev Pool <span>| Developer Search Platform</span>
+      </p>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni labore
+        sit inventore cumque deleniti distinctio vitae asperiores earum porro
+        eaque facilis ea culpa, est excepturi id cumque itaque esse doloremque!
+      </p>
+      <div className={styles.stack}>
+        {["Reactjs", "Nextjs", "Hardhat", "Etherjs", "Redux", "Electonjs"].map(
+          (skill, _i) => (
+            <span key={_i}>{skill}</span>
+          )
+        )}
+      </div>
     </div>
   );
 }
