280831f7ea001efa0cc044d1fadcca8948f52ba9
Harsh Pandey <coding.harshp@gmail.com>
2023-01-31 14:39:33 +0530

    Home Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 import Layout from "@/components/Layout";
 import { useEffect } from "react";
+import styles from "@/styles/Home.module.css";
+import ConnectWallet from "@/components/ConnectWallet";
 
 export default function Home() {
-  useEffect(() => {}, []);
-
-  return <Layout></Layout>;
+  return (
+    <Layout customHeader={<></>}>
+      <div className={styles.container}>
+        <ConnectWallet />
+      </div>
+    </Layout>
+  );
 }
 
-export const getServerSideProps = async (ctx) => {
-  // redirect test: always redirect to '/developers'
-  ctx.res.setHeader("Location", "/developers");
-  ctx.res.statusCode = 302;
-  ctx.res.end();
-  return {
-    props: {},
-  };
-};
+// export const getServerSideProps = async (ctx) => {
+//   ctx.res.setHeader("Location", "/developers");
+//   ctx.res.statusCode = 302;
+//   ctx.res.end();
+//   return {
+//     props: {},
+//   };
+// };
