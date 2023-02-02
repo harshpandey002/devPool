046c478510537ba40cbcad2e663256302999d85e
Harsh Pandey <coding.harshp@gmail.com>
2023-02-02 13:53:41 +0530

    Testing IPFS upload

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
+import { useRouter } from "next/router";
+import { useAccount, useNetwork } from "wagmi";
+import useIsMounted from "hooks/useIsMounted";
 
 export default function Layout({ children, customHeader }) {
+  const router = useRouter();
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const mounted = useIsMounted();
+
+  if (mounted && router.asPath != "/" && !address) {
+    router.push("/");
+  }
+
   return (
     <div className={styles.wrapper}>
       {customHeader ? customHeader : <Header />}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
-import Layout from "@/components/Layout";
-import { useEffect } from "react";
-import styles from "@/styles/Home.module.css";
 import ConnectWallet from "@/components/ConnectWallet";
-import { useNetwork, useSwitchNetwork } from "wagmi";
-import NetworkWarning from "@/components/NetworkWarning";
+import Layout from "@/components/Layout";
+import styles from "@/styles/Home.module.css";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
 
 export default function Home() {
+  const storage = new ThirdwebStorage();
+
+  const onClick = async () => {
+    try {
+      const payload = {
+        name: "NFT #1",
+        description: "This is my first NFT",
+        properties: [
+          {
+            name: "coolness",
+            value: "very cool",
+          },
+        ],
+      };
+
+      const uri = await storage.upload(payload);
+      const url = await storage.resolveScheme(uri);
+      console.log(url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout customHeader={<></>}>
       <div className={styles.container}>
         <ConnectWallet />
+        <button onClick={onClick}>Ipfs</button>
       </div>
     </Layout>
   );
