73fa6270d41b0098a2a0bd60e6fe59eaa3cbb38c
Harsh Pandey <coding.harshp@gmail.com>
2023-02-03 21:32:57 +0530

    Only Metamask Connect

--- components/ConnectWallet.jsx
+++ components/ConnectWallet.jsx
@@ -1,33 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import useIsMounted from "hooks/useIsMounted";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { useMetamask } from "@thirdweb-dev/react";
 
 export default function ConnectWallet() {
-  const { address, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
+  const connectWithMetamask = useMetamask();
 
-  const mounted = useIsMounted();
+  const onConnect = () => {
+    connectWithMetamask();
+  };
 
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect();
-  const connector = connectors[0];
-
-  if (!mounted) return null;
-
-  return isConnected ? (
-    <>
-      <p>{address}</p>
-      <button suppressHydrationWarning onClick={disconnect}>
-        Disconnect
-      </button>
-    </>
-  ) : (
-    <button
-      suppressHydrationWarning
-      disabled={!connector.ready}
-      onClick={() => connect({ connector })}
-    >
-      {connector.name}
-    </button>
-  );
+  return <button onClick={onConnect}>Metamask</button>;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import ConnectWallet from "@/components/ConnectWallet";
 import Layout from "@/components/Layout";
 import styles from "@/styles/Home.module.css";
-import { ConnectWallet } from "@thirdweb-dev/react";
-
+import { useAddress } from "@thirdweb-dev/react";
 export default function Home() {
+  const address = useAddress();
   return (
     <Layout customHeader={<></>}>
       <div className={styles.container}>
-        <ConnectWallet accentColor="#0d52a7" />
+        <p>{address}</p>
       </div>
     </Layout>
   );
