df2ecb29a5a4f3304c29793bb46524abbbf88e92
Harsh Pandey <coding.harshp@gmail.com>
2023-02-05 23:51:38 +0530

    Header Nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,11 @@
 import React from "react";
 import styles from "@/styles/Header.module.css";
 import Link from "next/link";
+import { useUserContext } from "@/context/userContext";
 
 export default function Header() {
+  const { user } = useUserContext();
+
   return (
     <div className={styles.header}>
       <Link href="/">
@@ -16,11 +19,19 @@ export default function Header() {
         <Link href="/inbox">
           <li>Inbox</li>
         </Link>
-        <Link href="/create">
-          <li>
-            <button>Create Profile</button>
-          </li>
-        </Link>
+        {user?.username ? (
+          <Link href={`/update/${user.username}`}>
+            <li>
+              <button>{user.username}</button>
+            </li>
+          </Link>
+        ) : (
+          <Link href="/create">
+            <li>
+              <button>Create Profile</button>
+            </li>
+          </Link>
+        )}
         {/* <li>
           <button id={styles.profile}>Harsh Pandey</button>
         </li> */}
