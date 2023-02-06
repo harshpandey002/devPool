77bb4f5f096b3a5fb4ec175e32c9d8a068dd97a2
Harsh Pandey <coding.harshp@gmail.com>
2023-02-06 14:00:57 +0530

    Landing Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
 import ConnectWallet from "@/components/ConnectWallet";
 import Layout from "@/components/Layout";
+import { useUserContext } from "@/context/userContext";
 import styles from "@/styles/Home.module.css";
-import { useAddress } from "@thirdweb-dev/react";
+import Link from "next/link";
+
 export default function Home() {
-  const address = useAddress();
+  const { user } = useUserContext();
   return (
-    <Layout customHeader={<></>}>
-      <div className={styles.container}>
-        <p>{address}</p>
-      </div>
-    </Layout>
+    <>
+      <Layout customHeader={<></>}>
+        <div id={styles.blobPurple} />
+        <div className={styles.container}>
+          <h1>
+            Create your decentralized <br /> <span>professional identity.</span>
+          </h1>
+          {user?.username ? (
+            <>
+              {/* <p>{user.username}</p> */}
+              <Link href="/developers">
+                <button>View Developers</button>
+              </Link>
+            </>
+          ) : (
+            <ConnectWallet />
+          )}
+          <img src="resume.png" alt="" />
+        </div>
+      </Layout>
+    </>
   );
 }
